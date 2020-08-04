f48a8e8641eb733f6aab775444935695d5af5d2d
cmayornavarro <cmayornavarro@gmail.com>
2020-08-04 08:14:22 +0200

    fisrt simple Promise

---
+++ firstPromise.js
@@ -0,0 +1,12 @@
+const promise =
+	new Promise((resolve,reject) => {
+		resolve("good");
+		//reject("bad");
+
+	})
+	.then(value =>{
+		console.log(value)
+	})
+	.catch(err =>{
+		console.log(err);
+	});
