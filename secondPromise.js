8b18234f193a18568358f9144b139d1e55fc41c4
cmayornavarro <cmayornavarro@gmail.com>
2020-08-04 08:14:55 +0200

    indented promises

---
+++ secondPromise.js
@@ -0,0 +1,23 @@
+const promise = new Promise((resolve, reject) => {
+	resolve("good");
+	//reject("bad");
+})
+	.then((value) => {
+		console.log(value);
+		return 1;
+	})
+	.then((value) => {
+		console.log(value);
+		return 2;
+	})
+	.then((value) => {
+		console.log(value);
+		return 3;
+	})
+	.then((value) => {
+		console.log(value);
+		return 4;
+	})
+	.catch((err) => {
+		console.log(err);
+	});
